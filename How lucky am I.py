57bba1488fcf05aa43b1115de459181085abcc55
ArshiaMadadii <arshiamadadi2002@gmail.com>
2025-01-24 07:57:51 -0800

    hey...

--- How lucky am I.py
+++ How lucky am I.py
@@ -1,6 +1,26 @@
 import streamlit as st
 import pandas as pd
 import random
+import http.server
+import socketserver
+import os
+
+PORT = 8501
+DIRECTORY = "."
+
+class CustomHandler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+    def do_GET(self):
+        if self.path == "/":
+            self.path = "/index.html"
+        return super().do_GET()
+
+with socketserver.TCPServer(("", PORT), CustomHandler) as httpd:
+    print(f"Serving on port {PORT}")
+    httpd.serve_forever()
+
 
 # Function to load custom fonts
 def load_custom_fonts():
